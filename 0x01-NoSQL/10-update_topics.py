3d305e5cffb3a7acaa195116c4504ba6479b414e
JSM788 <salinasjuan788@gmail.com>
2022-12-20 15:56:58 -0500

    Adding 10-update_topics.py file

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Defining the function update_topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """This function changes all topics of a school document based on the name"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
