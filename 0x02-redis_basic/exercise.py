0e3d4e71be85c6b39e715ca6e12d69819178ae10
JSM788 <salinasjuan788@gmail.com>
2022-12-21 12:26:57 -0500

    Adding data types

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """Defining the class Cache"""
 from uuid import uuid4
 import redis
+from typing import Union
 
 
 class Cache():
@@ -11,7 +12,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """This method take a data and returns a string"""
         key = str(uuid4())
         self._redis.set(key, data)
